a96543c39b57a2a997f8adfca971df7cfd53fd0a
Aleksei <saty_tan@mail.ru>
2018-07-12 00:40:59 +0300

    Нерабочий вариант для тестов - проверка динамического отображения страницы.

--- client/main.js
+++ client/main.js
@@ -8,10 +8,33 @@ import { LotteryCreate } from './import/LotteryCreate.html'
 import { LotteryContent } from './import/LotteryContent.html'
 import { LotteryList } from './import/LotteryList.html'
 
+import { LotteryListActivePage } from './pages/LotteryListActivePage.html'
+import { MainPage } from './pages/MainPage.html'
+
 
 
 import './main.html';
 import './style/bootstrap.min.css';
 import './js/bootstrap.min.js';
+
+Template.MainContent.onCreated(function helloOnCreated() {
+    // counter starts at 0
+    this.page = new ReactiveVar(0);
+});
+
+
+Template.MainContent.drawPage = function () {
+    return "Test";
+};
+// Template.MainContent.helpers({
+//     drawPage:function () {
+//         return "Test";
+//         // return Template.MainPage;
+//         // let page =  Template.instance().counter.get();
+//         // if(page == 0)
+//         //     return Template.MainPage;
+//         // else return Template.LotteryListActivePage;
+//     }
+// });
 // import './style/style.import.less';
 
