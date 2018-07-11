176f155d66be6cac01430f06bfb202fcc171efdf
saty_tan@mail.ru <tmp4pass>
2018-07-11 14:04:43 +0300

    Добавлена структура для добавления стилей в less Добавлен статичный блок с рекомендуемыми лотереями

--- client/main.js
+++ client/main.js
@@ -1,12 +1,15 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import { Navbar } from './import/header.html';
+import { AboutArticle } from './import/AboutArticle.html'
+import { LotterySet } from './import/LotterySet.html'
+
 
 
 import './main.html';
 import './style/bootstrap.min.css';
 import './js/bootstrap.min.js';
-import './style/style.import.less';
+// import './style/style.import.less';
 
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
