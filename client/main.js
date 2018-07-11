b43865f60983b2d7c0ac022536be3362534496df
saty_tan@mail.ru <tmp4pass>
2018-07-11 14:39:37 +0300

    Добавлена секция с общей статистикой Добавлены стили для новых элементов Добавлены в less переменные для управления цветами сайта

--- client/main.js
+++ client/main.js
@@ -3,6 +3,7 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import { Navbar } from './import/header.html';
 import { AboutArticle } from './import/AboutArticle.html'
 import { LotterySet } from './import/LotterySet.html'
+import { LotteryTotalInfo } from './import/LotteryTotalInfo.html'
 
 
 
@@ -11,20 +12,3 @@ import './style/bootstrap.min.css';
 import './js/bootstrap.min.js';
 // import './style/style.import.less';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
